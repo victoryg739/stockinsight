910f4ab104579f3fd57cada438568f49528991c5
victor <victoryoong570@gmail.com>
2024-08-22 11:47:53 +0800

    nothing change

--- src/app/components/Alert.tsx
+++ src/app/components/Alert.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 type AlertProps = {
   message: string;
 };
+
 export default function Alert({ message }: AlertProps) {
   const [isVisible, setIsVisible] = useState(true);
   useEffect(() => {
